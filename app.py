b628dbb0c1df3ea1906bced6691ea34e35246daa
farProgrammer <72961644+farProgrammer@users.noreply.github.com>
2021-06-05 20:12:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def list_products():
 @app.route("/search/<product_brand>",methods=["GET","POST"])
 def List_product_by_brand(product_brand):
     """List product_brand."""
-    res = requests.get('http://makeup-api.herokuapp.com/api/v1/products.json?brand={product_brand}')
+    res = requests.get(f 'http://makeup-api.herokuapp.com/api/v1/products.json?brand={product_brand}')
    
     return render_template("product_brand.html",res=res)
 
